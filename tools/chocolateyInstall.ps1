6ef469c3902ac9cea4ea3bc257bbd2273ae3b99d
Patrick Hufford <angryhussord@outlook.com>
2015-08-06 23:17:21 -0700

    New package for OBS MP
    OBS Multiplatform release 0.11.2

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'obs_mp'
+$installerType = 'EXE'
+$url = 'https://github.com/jp9000/obs-studio/releases/download/0.11.2/OBS-MP-0.11.2-Installer.exe'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
