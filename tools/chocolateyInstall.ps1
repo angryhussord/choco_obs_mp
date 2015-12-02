3f912913382dc59a212b5b13e126856174acd7dc
Patrick Hufford <pahuff@microsoft.com>
2015-12-02 02:12:49 -0800

    Update for 0.12.2 release

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'obs_mp'
 $installerType = 'EXE'
-$url = 'https://github.com/jp9000/obs-studio/releases/download/0.12.0/OBS-MP-0.12.0-Installer.exe'
+$url = 'https://github.com/jp9000/obs-studio/releases/download/0.12.2/OBS-MP-0.12.2-Installer.exe'
 $silentArgs = '/S'
 $validExitCodes = @(0)
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
