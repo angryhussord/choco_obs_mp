a2ddf8af5e2b8ba78466a40f5c5de58a93994c87
Patrick Hufford <pahuff@microsoft.com>
2016-03-24 07:50:50 -0700

    Updates for 0.13.4 and the new guidelines

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,6 +1,7 @@
 ﻿$packageName = 'obs-mp'
 $installerType = 'EXE'
-$url = 'https://github.com/jp9000/obs-studio/releases/download/0.13.2/OBS-Studio-0.13.2-Installer.exe'
+$url = 'https://github.com/jp9000/obs-studio/releases/download/0.13.4/OBS-Studio-0.13.4-Installer.exe'
+$url64 = $url;
 $silentArgs = '/S'
 $validExitCodes = @(0)
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes
