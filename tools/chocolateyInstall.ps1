c023f114681f16368c8fbf8eaf47655a2eab40cd
Patrick Hufford <pahuff@microsoft.com>
2016-11-14 10:24:56 -0800

    deprecating package for obs-studio after rename by author

--- tools/chocolateyInstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿$packageName = 'obs-mp'
-$installerType = 'EXE'
-$url = 'https://github.com/jp9000/obs-studio/releases/download/0.14.2/OBS-Studio-0.14.2-Installer.exe'
-$url64 = $url
-$silentArgs = '/S'
-$validExitCodes = @(0)
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes
